11a4e0c1afc09297fee5de3c01d910eda6df7671
sujane <sujane_mf@mail.ru>
2022-07-12 22:11:18 +0300

    Initial commit

---
+++ src/main/java/ru/geekbrains/SecurityLesson11/SecurityLesson11Application.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.SecurityLesson11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityLesson11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityLesson11Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/geekbrains/SecurityLesson11/SecurityLesson11ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.SecurityLesson11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityLesson11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
